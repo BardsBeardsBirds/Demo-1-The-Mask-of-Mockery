254b85e1b0e383e57cdceac15e7010b798c2132b
Sezer <bardsbeardsbirds@gmail.com>
2015-05-15 17:50:56 +0200

    Introduced a water ring effect if the player walks in shallow water

---
+++ Assets/Scripts/Emmon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Emmon : MonoBehaviour
+{
+    public static Emmon Instance;
+
+    public GameObject WaterRings = null;
+    public GameObject InWaterGo;
+    public bool InWater = false;
+
+    public void Awake()
+    {
+        Instance = this;
+
+        SetWaterRings();
+    }
+
+    public void Update()
+    {
+        if(InWater)
+        {
+            WaterRings.transform.position = new Vector3(WaterRings.transform.position.x,
+                InWaterGo.GetComponent<WaterBehaviour>().WaterLevel, 
+                WaterRings.transform.position.z);
+        }
+    }
+
+    public void SetWaterRings()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.name == "Particle Effects")
+            {
+                GameObject waterRings = GameObject.Instantiate(Resources.Load("Prefabs/Particles/Water Rings") as GameObject);
+                waterRings.transform.SetParent(child);
+                waterRings.transform.position = child.position;
+                WaterRings = waterRings;
+                WaterRings.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Locations/WaterBehaviour.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+public class WaterBehaviour : MonoBehaviour
+{
+    public Vector3 WaterAboveObject;
+    public float WaterLevel;
+    private float _waterRingDuration = 8f;
+    private float _waterTimer = 0;
+
+    public void Awake()
+    {
+   //         Debug.DrawRay(this.transform.position, WaterAboveObject, Color.magenta);
+        WaterAboveObject = new Vector3(0, 0.85f, 0);
+        WaterLevel = WaterAboveObject.y + this.transform.position.y;
+    }
+
+    public void Update()
+   {
+    //    Debug.DrawRay(this.transform.position, WaterAboveObject, Color.magenta);
+    //    Debug.Log(this.transform.position.y);
+        if(_waterTimer > 0)
+        {
+            _waterTimer -= Time.deltaTime;
+            if (_waterTimer <= 0)
+            {
+                if (Emmon.Instance.InWater)
+                {
+                    Debug.Log("We finished the timer but are in the water");
+                }
+                else  
+                {
+                    Emmon.Instance.WaterRings.SetActive(false);
+                    Debug.Log("We finished the timer and are not in the water");
+                }
+            }
+        }
+            
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+            return;
+
+        Emmon.Instance.InWaterGo = this.gameObject;
+
+        Emmon.Instance.InWater = true;
+        Emmon.Instance.WaterRings.SetActive(true);
+        Emmon.Instance.WaterRings.GetComponentInChildren<ParticleSystem>().emissionRate = 8;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Player")
+            return;
+
+        Emmon.Instance.InWater = false;
+
+        _waterTimer = _waterRingDuration;
+
+        Emmon.Instance.WaterRings.GetComponentInChildren<ParticleSystem>().emissionRate = 0;
+
+     //   StartCoroutine(TurnOffWater());
+
+
+    }
+
+
+    //public IEnumerator TurnOffWater()
+    //{
+        
+    //    Debug.Log("We are in the routine");
+
+    //    yield return new WaitForSeconds(_waterRingDuration);
+
+
+    //}
+}
